472ace190099de409b07442bca0dc78e564c5fa6
Gustavo Henrique <gustavohenriquegithub@gmail.com>
2024-07-29 13:43:20 -0300

    formulario de login e componente input

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,5 +1,7 @@
 import Input from "../input";
 
+import { CiSearch } from "react-icons/ci";
+
 function Header() {
   return (
     <div className="flex justify-between p-4 items-center">
@@ -10,13 +12,15 @@ function Header() {
           className="h-10"
         />
       </div>
-      <div className="flex w-1/2 border-black border-y-2 border-x-2 p-1">
+      <div className="flex w-1/2 rounded-lg border-black border-y-2 border-x-2 p-1">
       <Input
           type="text"
           styles="w-full bg-transparent border-none outline-none"
           placeholderName="Digite o nome de um perfil, postagens..."
         />
-        <button>pesquisar</button>
+        <button className="w-10 flex justify-center items-center">
+          <CiSearch />
+        </button>
       </div>
       <div>
         <div className="rounded-full bg-black">Foto de perfil</div>
